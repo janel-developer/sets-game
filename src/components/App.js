4da71a94b9a5ca229522a16473f2d0b2d20b6b8d
janelbrandon <janel.brandon@coderacademy.edu.au>
2020-01-19 14:26:48 +1000

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,63 @@
+import React from "react"
+import styled from "styled-components"
+import SetsCard from "./SetsCard"
+
+const App = () => {
+    const innerWidth = window.innerWidth;
+    let width = "100vw"
+    if(innerWidth > 620)
+        width = "620px"
+
+    const Game = styled.div `
+        display: grid;
+        border-size: 3px;
+    `
+    const Heading = styled.div `
+        text-align: center;
+        font-family: 'Amarante', cursive;
+        font-size: 3em;
+        background: url("img/sacred-geo.svg");
+        background-size: 40%;
+        background-color: rgba(0,200,255,.5);
+        color: darkblue;
+        margin: .5em;
+        height: 10vh;
+    `
+    const GameBoard = styled.div `
+        display: flex;
+        justify-content: center;
+    `
+    const Cards = styled.div `
+        display: flex;
+        flex-wrap: wrap;
+        width: ${width};
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+    `
+    return (
+        <Game>
+            <Heading >Sets</Heading>
+        <GameBoard >
+        <Cards>
+            <SetsCard color="darkblue" shape="circle" number={[1]} fill="solid" />
+            <SetsCard color="red" shape="square" number={[1,2]} fill="empty" />
+            <SetsCard color="black" shape="circle" number={[1,2]} fill="fill" />
+            <SetsCard color="darkblue" shape="oval" number={[1]} fill="fill" />
+            <SetsCard color="red" shape="circle" number={[1,2,3]} fill="empty" />
+            <SetsCard color="black" shape="square" number={[1,2,3]} fill="empty" />
+            <SetsCard color="darkblue" shape="square" number={[1]} fill="fill" />
+            <SetsCard color="red" shape="oval" number={[1,2,3]} fill="empty" />
+            <SetsCard color="black" shape="circle" number={[1,2,3]} fill="empty" />
+            <SetsCard color="darkblue" shape="square" number={[1,2]} fill="fill" />
+            <SetsCard color="black" shape="circle" number={[1,2,3]} fill="fill" />
+            <SetsCard color="red" shape="oval" number={[1,2,3]} fill="fill" />
+
+        </Cards>
+        </GameBoard>
+        <Heading />
+        </Game>
+    )
+}
+
+export default App
---
+++ src/components/SetsCard.js
@@ -0,0 +1,44 @@
+import React from "react"
+import styled from "styled-components"
+
+const SetsCard = ({color, number, shape, fill}) => {
+    const innerWidth = window.innerWidth
+    const cardWidth = innerWidth > 500 ? 150 : 75
+    const cardHeight = innerWidth > 500 ? 300 : 150
+    let shapeWidth = innerWidth > 500 ? 100 : 50
+    if (shape !== "circle") 
+        shapeWidth = shapeWidth / 2
+    const shapeHeight = innerWidth > 500 ? 100: 50 
+    const borderRadius = (shape === "circle") ? "50%" : "50px / 100px"
+
+    const Card = styled.div `
+        border: 2px solid black;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-content: center;
+        height: ${cardHeight}px;
+        width: ${cardWidth}px;
+    `
+    const CardShape = styled.span `
+        height: ${shapeHeight/number.length}px;
+        width: ${shapeWidth/number.length}px;
+        background: ${color};
+        border: 2px solid ${color};
+        border-radius: ${(shape === "square") ? "0" : borderRadius};
+        mask-size: 200%;
+        mask-position: center;
+        margin: .5em;
+    `
+   return (
+    <Card>
+        { number.map((key) => 
+            (fill === "fill") 
+            ? <CardShape key={key} className="fill" /> 
+            : <CardShape key={key} />
+        )}
+    </Card>
+   )          
+}
+
+export default SetsCard
