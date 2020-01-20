9249842ab32adf4fe1dbd9d9ea0d67714e03320d
janelbrandon <janel.brandon@coderacademy.edu.au>
2020-01-20 14:32:15 +1000

    add comments and fix showing empty shapes

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,20 @@ import SetsCard from "./SetsCard"
 const App = () => {
     const innerWidth = window.innerWidth;
     let width = "100vw"
+
+    // If a medium/large screen, use 620px wide board
+    // If small/xs screen, use 100vw
     if(innerWidth > 620)
         width = "620px"
 
+    // Centers the gameboard horizontally on the screen
+    // and gives it a border
     const Game = styled.div `
         display: grid;
         border-size: 3px;
     `
+
+    // Styles the heading
     const Heading = styled.div `
         text-align: center;
         font-family: 'Amarante', cursive;
@@ -23,10 +30,15 @@ const App = () => {
         margin: .5em;
         height: 10vh;
     `
+
+    // Centers the cards horizonally on the gameboard
     const GameBoard = styled.div `
         display: flex;
         justify-content: center;
     `
+
+    // Centers cards horizontally and vertically and
+    // places them in columns of 4 cards in all resolutions
     const Cards = styled.div `
         display: flex;
         flex-wrap: wrap;
@@ -45,9 +57,9 @@ const App = () => {
             <SetsCard color="black" shape="circle" number={[1,2]} fill="fill" />
             <SetsCard color="darkblue" shape="oval" number={[1]} fill="fill" />
             <SetsCard color="red" shape="circle" number={[1,2,3]} fill="empty" />
-            <SetsCard color="black" shape="square" number={[1,2,3]} fill="empty" />
+            <SetsCard color="black" shape="square" number={[1,2,3]} fill="solid" />
             <SetsCard color="darkblue" shape="square" number={[1]} fill="fill" />
-            <SetsCard color="red" shape="oval" number={[1,2,3]} fill="empty" />
+            <SetsCard color="red" shape="oval" number={[1,2,3]} fill="solid" />
             <SetsCard color="black" shape="circle" number={[1,2,3]} fill="empty" />
             <SetsCard color="darkblue" shape="oval" number={[1,2]} fill="fill" />
             <SetsCard color="black" shape="circle" number={[1,2,3]} fill="fill" />
--- src/components/SetsCard.js
+++ src/components/SetsCard.js
@@ -2,15 +2,28 @@ import React from "react"
 import styled from "styled-components"
 
 const SetsCard = ({color, number, shape, fill}) => {
+    // Get inner width of the window
     const innerWidth = window.innerWidth
+    // Cards are 150px wide for md/large screen, and 75px wide for sm/xs screen
     const cardWidth = innerWidth > 500 ? 150 : 75
+    // Cards are 300px high for md/large screen, and 150px high for sm/xs screen
     const cardHeight = innerWidth > 500 ? 300 : 150
+    // Card shapes are 100px wide for md/large screen and 50px wide for sm/xs screen
+    // If there are 2 shapes, divide width by 2 (below)
+    // If there are 3 shapes, divide width by 3 (below)
     let shapeWidth = innerWidth > 500 ? 100 : 50
+    // If a rectangle or oval, width is half of height
     if (shape !== "circle") 
         shapeWidth = shapeWidth / 2
+    // Card shapes are 100px tall for md/large screen and 50px tall for sm/xs screen
+    // If there are 2 shapes, divide height by 2 (below)
+    // If there are 3 shapes, divide height by 3 (below)
     const shapeHeight = innerWidth > 500 ? 100: 50 
+    // Set border radius for circle and oval
     const borderRadius = (shape === "circle") ? "50%" : "50px / 100px"
 
+    // Styles the card with a black border
+    // Centers shapes on the card
     const Card = styled.div `
         border: 2px solid black;
         display: flex;
@@ -21,10 +34,14 @@ const SetsCard = ({color, number, shape, fill}) => {
         height: ${cardHeight}px;
         width: ${cardWidth}px;
     `
+
+    // Creates the card shapes and fill
+    // Uses mask to fill with svg of the appropriate color
+    // SVG image is defined in style sheet
     const CardShape = styled.span `
         height: ${shapeHeight/number.length}px;
         width: ${shapeWidth/number.length}px;
-        background: ${color};
+        background: ${(fill === "empty") ? "white" : color};
         border: 2px solid ${color};
         border-radius: ${(shape === "square") ? "0" : borderRadius};
         mask-size: 200%;
