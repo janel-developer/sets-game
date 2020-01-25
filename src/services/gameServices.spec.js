cea3d5dd4a0b8dd2c5cdb1b3f9888c6f2f529726
janelbrandon <janel.brandon@coderacademy.edu.au>
2020-01-25 15:29:00 +1000

    implemented selectedCardsAreSet with tests

--- src/services/gameServices.spec.js
+++ src/services/gameServices.spec.js
@@ -1,4 +1,4 @@
-import {newCardDeck, deal} from "./gameServices"
+import {newCardDeck, deal, selectedCardsAreSet} from "./gameServices"
 import {colors, numbers, shapes, fills} from "./gameConstants"
 
 let fixtures = {}
@@ -46,5 +46,89 @@ describe("deal", () => {
         dealtCards = deal(fixtures.deck,12)
         expect(dealtCards.length).toEqual(12)
         expect(fixtures.deck.length).toEqual(81-12)
+    })
+})
+describe("selectedCardsAreSet", () => {
+    it("returns false when less than 3 cards", () => {
+        const selectedCards = ["1|red|circle|solid","1|black|circle|solid"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+    })
+    it("returns false when exactly 2 of 3 cards have same number", () => {
+        let selectedCards = ["1|black|circle|fill","1|black|circle|solid","2|black|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+        selectedCards = ["1|black|circle|fill","2|black|circle|solid","2|black|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+        selectedCards = ["1|black|circle|fill","3|black|circle|solid","3|black|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+        selectedCards = ["2|black|circle|fill","3|black|circle|solid","3|black|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+        selectedCards = ["2|black|circle|fill","2|black|circle|solid","3|black|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+    })
+    it("returns false when exactly 2 of 3 three colors are the same", () => {
+        let selectedCards = ["1|red|circle|fill","1|black|circle|solid","1|black|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+        selectedCards = ["1|black|circle|fill","2|red|circle|solid","3|black|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+        selectedCards = ["1|black|circle|fill","2|red|circle|solid","3|red|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+    })
+    it("returns false when exactly 2 of 3 shapes are the same", () => {
+        let selectedCards = ["1|black|circle|fill","1|black|circle|solid","1|black|square|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+        selectedCards = ["1|black|circle|fill","1|black|square|solid","1|black|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+        selectedCards = ["1|black|square|fill","1|black|circle|solid","1|black|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+
+    })
+    it("returns false when exactly 2 of the 3 fills are the same",() => {
+        let selectedCards = ["1|black|circle|fill","1|black|oval|fill","1|black|square|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+        selectedCards = ["1|black|circle|fill","1|black|oval|empty","1|black|square|fill"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+        selectedCards = ["1|black|circle|fill","1|black|oval|empty","1|black|square|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(false)
+
+    })
+    it("returns true when we have 3 like numbers, 3 like colors, 3 like shapes, 3 different fills", () => {
+        const selectedCards = ["1|black|circle|solid","1|black|circle|fill","1|black|circle|empty"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(true)
+    })
+    it("returns true when we have 3 like numbers, 3 like colors, 3 like fills, 3 different shapes", () => {
+        const selectedCards = ["1|black|circle|solid","1|black|oval|solid","1|black|square|solid"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(true)
+    })
+    it("returns true when we have 3 like numbers, 3 like shapes, 3 like fills, 3 different colors", () => {
+        const selectedCards = ["1|black|circle|solid","1|red|circle|solid","1|darkblue|circle|solid"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(true)
+    })
+    it("returns true when we have 3 like colors, 3 like shapes, 3 like fills, 3 different numbers", () => {
+        const selectedCards = ["1|black|circle|solid","2|black|circle|solid","3|black|circle|solid"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(true)
+    })
+    it("returns true when we have 3 different numbers, 3 different colors, 3 different shapes, 3 like fills", () => {
+        const selectedCards = ["1|black|circle|solid","2|red|square|solid","3|darkblue|oval|solid"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(true)
+    })
+    it("returns true when we have 3 different numbers, 3 different colors, 3 different fills, 3 like shapes", () => {
+        const selectedCards = ["1|black|circle|solid","2|red|circle|empty","3|darkblue|circle|fill"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(true)
+    })
+    it("returns true when we have 3 different numbers, 3 different shapes, 3 different fills, 3 like colors", () => {
+        const selectedCards = ["1|black|circle|solid","2|black|square|empty","3|black|oval|fill"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(true)
+    })
+    it("returns true when we have 3 different colors, 3 different shapes, 3 different fills, 3 like numbers", () => {
+        const selectedCards = ["1|black|circle|solid","1|red|square|empty","1|darkblue|oval|fill"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(true)
+    })
+    it("returns true when we have 3 like colors, 3 different shapes, 3 like fills, 3 different numbers", () => {
+        const selectedCards = ["1|black|circle|solid","2|black|square|solid","3|black|oval|solid"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(true)
+    })
+    it("returns true when we have 3 different colors, 3 like shapes, 3 different fills, 3 like numbers", () => {
+        const selectedCards = ["1|black|circle|solid","1|red|circle|empty","1|darkblue|circle|fill"]
+        expect(selectedCardsAreSet(selectedCards)).toEqual(true)
     })
 })
