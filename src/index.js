649880e1b2fd11e7fd8d7524c13d8e10a933332c
janelbrandon <janelbrandon@microsoft.com>
2025-02-09 16:57:31 +1000

    update dependencies after a long itme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react"
-import ReactDOM from "react-dom"
 import App from "./components/App"
 import "./style/style.scss"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+
+
+// This is the ID of the div in your index.html file
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
